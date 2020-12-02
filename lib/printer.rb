59439c1e29e9c3aacca58fbe65c0a45d9329678c
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-02 13:18:35 +0000

    Formatting bug fixed with spacing in printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,7 +2,7 @@ class Printer
   def print(statement)
     print_header
     statement.reverse.each do |transaction|
-      puts "#{transaction.date} || #{format(transaction.credit)} || #{format(transaction.debit)} || #{format(transaction.balance)}"
+      puts "#{transaction.date} ||#{format(transaction.credit)} ||#{format(transaction.debit)} ||#{format(transaction.balance)}"
     end
   end
 
@@ -12,7 +12,7 @@ class Printer
 
   def format(transaction_value)
     if !transaction_value.nil?
-      "%.2f" % transaction_value
+      " " + "%.2f" % transaction_value
     end
   end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -5,8 +5,8 @@ require 'transaction'
 describe 'BankAccount' do
   let(:account) { BankAccount.new }
   let(:header) { "date || credit || debit || balance\n" }
-  let(:first_transaction) { "02/12/2020 || 50.00 ||  || 50.00\n" }
-  let(:second_transaction) { "02/12/2020 ||  || 30.00 || 20.00\n" }
+  let(:first_transaction) { "02/12/2020 || 50.00 || || 50.00\n" }
+  let(:second_transaction) { "02/12/2020 || || 30.00 || 20.00\n" }
   it 'transactions are printed in reverse chronological order' do
     account.deposit(50)
     account.withdraw(30)
