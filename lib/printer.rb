3d741878ebd16ec1e14afc219556cba32ec80133
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-02 11:55:58 +0000

    Printer can print statements containing transactions

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,9 @@
 class Printer 
   def print(statement)
     print_header
+    statement.reverse.each do |transaction|
+      puts "#{transaction.date} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"
+    end
   end
 
   def print_header
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,9 +1,14 @@
 require 'printer'
 
 describe Printer do
+  let(:transaction) { double :transaction, :date => '01/01/1010', :credit => '50', :debit => nil, :balance => '50'}
   describe '#print' do
     it 'prints a header' do
       expect { subject.print([]) }.to output("date || credit || debit || balance\n").to_stdout
+    end
+
+    it 'prints the statement' do
+      expect { subject.print([transaction]) }.to output("date || credit || debit || balance\n01/01/1010 || 50 ||  || 50\n").to_stdout
     end
   end
 end
