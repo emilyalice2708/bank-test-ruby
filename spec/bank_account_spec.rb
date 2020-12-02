54d31e3a798fb6b377beec86e2112408a41495c0
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-02 10:29:43 +0000

    Rubocop and simplecov installed

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,9 +1,16 @@
 require 'bank_account'
 
 describe 'BankAccount' do
-  let(:account) {BankAccount.new}
+  let(:account) { BankAccount.new }
+  let(:transaction) { double :transaction }
 
   it 'has an empty balance on initialization' do
     expect(account.balance).to eq 0
+  end
+
+  describe 'deposit' do
+    it 'creates a new transaction' do
+      
+    end
   end
 end
